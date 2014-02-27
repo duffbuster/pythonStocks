c3a545c51a03520392b346071050bc3c05ab3015
Colin Mackey <colin@fishbrains.com>
2014-02-27 15:59:23 -0500

    Create stocks.sql
    Sql file that sets up the user table structure for the database

---
+++ stocks.sql
@@ -0,0 +1,16 @@
+DROP DATABASE stockTickers;
+CREATE DATABASE IF NOT EXISTS stockTickers;
+GRANT ALL PRIVILEGES ON stockTickers.* to 'user'@'localhost' 
+identified by 'password';
+USE stockTickers;
+
+--Set up initial user table structure
+
+CREATE TABLE users
+(
+  firstName VARCHAR(256),
+  lastName VARCHAR(256),
+  username VARCHAR(256);
+  password VARCHAR(256);
+  PRIMARY KEY (username);
+);
