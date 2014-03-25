a0e6d18112442655e363e0fff4a90b17875d7163
duffbuster <colin@fishbrains.com>
2014-03-25 12:44:54 -0400

    commenting out

--- server.py
+++ server.py
@@ -12,10 +12,10 @@ app.secret_key = 'Zq4oA4Dqq3'
 # Homepage: search for stocks and get current price
 @app.route('/', methods=['GET', 'POST'])
 def mainIndex():
-    ticker = request.form['symbol']
-    stockPrice = ystockquote.get_price(ticker)
+    #ticker = request.form['symbol']
+    #stockPrice = ystockquote.get_price(ticker)
     
-    return render_template('index.html', selectedMenu='Home', stockPrice=stockPrice)
+    return render_template('index.html', selectedMenu='Home')
 
 # TODO: addStock route
 
